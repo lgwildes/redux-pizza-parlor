0f50a697622c3809dbc9b7a71487521eb1820ad3
Lydia Wildes <lydiawildes1@gmail.com>
2022-10-27 10:07:26 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, bindActionCreators } from 'redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pizzaList = (state=[], action) => {
+    //TODO get pizzaList from database
+}
+
+const order = (state={}, action) => {
+    //TODO add customer info and pizza order from form/cart info idk
+}
+
+const cart = (state=[], action) => {
+    //TODO add pizza objects from form 
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        pizzaList,
+        order,
+        cart
+
+    }),
+    applyMiddleware(logger)
+);
+
+
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
